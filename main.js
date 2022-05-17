ee71c8235e6f51f939fc014ea03654bba640489b
AshkanKavianpour <91813322+AshkanKavianpour@users.noreply.github.com>
2022-05-16 18:05:00 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+noseX=0;
+noseY=0;
+
+
+function preload() {
+    clown_nose=loadImage('https://i.postimg.cc/zffp8F4r/moustache-png-17164.png');
+}
+
+function setup() {
+    canvas=createCanvas(300, 300);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+    
+}
+
+function modelLoaded() {
+    console.log('Posenet is Initialized');
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        noseX=results[0].pose.nose.x-20;
+        noseY=results[0].pose.nose.y;
+        
+        console.log("Nose x = " + noseX);
+        console.log("Nose y = " + noseY);
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+
+ 
+
+    image(clown_nose, noseX, noseY, 40, 40);
+}
+
+function take_snapshot() {
+    save('myfilterimage.png');
+
+}
